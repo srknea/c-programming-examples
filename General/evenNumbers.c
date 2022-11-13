15cff166c8b19668d19adb27a5bf3f2e7cf8913b
Serkan ISIK <dehnas79@gmail.com>
2022-11-13 18:39:08 +0300

    Create evenNumbers.c

---
+++ General/evenNumbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+//Write all even numbers between 1 and 20 except 14.
+
+int main(){
+	
+	int i;
+	i=2;
+	while(i<=20)
+	{
+		if(i!=14)
+		{
+		printf("%d\n",i);
+		}
+		i+=2;
+		
+	}
+	
+	
+return 0;
+}
+
+// 1 ile 20 arasindaki 14 haric tum cift sayilari yazdiran programin kodu.
