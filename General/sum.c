8756c08707f18869015a09e40b8f678b759286a7
Serkan ISIK <dehnas79@gmail.com>
2022-11-21 18:28:44 +0300

    Create sum.c

---
+++ General/sum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Find the sum of the first 50 natural numbers.
+
+int main(){
+	
+	int i,toplam;
+	
+	for(i=0;i<=49;i++)
+	{
+	toplam+=i;	
+	}	
+	
+	printf("Ilk elli dogal sayinin toplami: %d",toplam);
+	
+		
+return 0;	
+}
+
+// Draw a flowchart to find the sum of the first 50 natural numbers.
